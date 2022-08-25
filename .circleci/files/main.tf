501a66bebdefe6d2b790a0e9be28a68cb27aba5d
Paul <freemeonline@gmail.com>
2022-08-25 21:11:36 +0100

    Added terraform to deploy-infrastructure job

--- .circleci/files/main.tf
+++ .circleci/files/main.tf
@@ -14,28 +14,29 @@ provider "aws" {
 }
 
 resource "aws_security_group" "my_sec_group" {
-	ingress {
-	  cidr_blocks = [ "0.0.0.0/0" ]
-	  description = "accept ssh rule"
-	  from_port = 22
-	  protocol = "tcp"
-	  to_port = 22
-	}
-	tags = {
-	  "Name" = "${var.ID}-Microservice"
-	}
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "accept ssh rule"
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+  }
+  tags = {
+    "Name" = "${var.ID}-Microservice"
+  }
 }
 
 resource "aws_instance" "TestInstance" {
-  	ami = "ami-052efd3df9dad4825"
-	instance_type = "t2.micro"
-	security_groups = [aws_security_group.my_sec_group.name]
-	associate_public_ip_address = true
-	key_name = "eks"
-	tags = {
-	  "Name" = "${var.ID}-Microservice"
-	}
-	provisioner "local-exec" {
-		command = "echo ${self.public_ip} >> ~/.circleci/ansible/inventory.txt"
-	}
+  ami                         = "ami-052efd3df9dad4825"
+  instance_type               = "t2.micro"
+  security_groups             = [aws_security_group.my_sec_group.name]
+  associate_public_ip_address = true
+  key_name                    = "eks"
+  tags = {
+    "Name" = "${var.ID}-Microservice"
+  }
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> ~/.circleci/ansible/inventory.txt"
+  }
 }
+
--- .circleci/files/variables.tf
+++ .circleci/files/variables.tf
@@ -1,9 +1,9 @@
 variable "ID" {
-  
+
 }
 variable "TF_VAR_AWS_ACCESS_KEY_ID" {
-  
+
 }
 variable "TF_VAR_AWS_SECRET_ACCESS_KEY" {
-  
+
 }
