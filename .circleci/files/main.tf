47d828e9be11ce870b7fd4315f43d0de214da86d
Paul <freemeonline@gmail.com>
2022-08-25 21:14:53 +0100

    Added terraform to deploy-infrastructure job

--- .circleci/files/main.tf
+++ .circleci/files/main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "TestInstance" {
     "Name" = "${var.ID}-Microservice"
   }
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} >> ~/.circleci/ansible/inventory.txt"
+    command = "echo ${self.public_ip} >> ~/project/.circleci/ansible/inventory.txt"
   }
 }
 
